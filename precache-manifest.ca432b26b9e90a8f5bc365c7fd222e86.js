b1ea3a6d929edb2c66860f6efbc4adfffd3f3d65
siddhantparadox <siddhantg314@gmail.com>
2020-09-16 14:24:44 +0530

    Updates

---
+++ precache-manifest.ca432b26b9e90a8f5bc365c7fd222e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845218aecdec5548c73299bd249c9838",
+    "url": "/food-recipe-react/index.html"
+  },
+  {
+    "revision": "1926d9b9b464ba9287e4",
+    "url": "/food-recipe-react/static/css/main.aee87b58.chunk.css"
+  },
+  {
+    "revision": "05a59d2afcc6e1dcdf79",
+    "url": "/food-recipe-react/static/js/2.25a0e88b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe-react/static/js/2.25a0e88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1926d9b9b464ba9287e4",
+    "url": "/food-recipe-react/static/js/main.59469ec9.chunk.js"
+  },
+  {
+    "revision": "40cd236afbb11b694874",
+    "url": "/food-recipe-react/static/js/runtime-main.ea896919.js"
+  }
+]);
